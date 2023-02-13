c0de1c46d7f54c99f39e37ce45afa4a591ad89eb
sang ah <kr.sangah@gmail.com>
2023-02-13 09:31:56 +0900

    230213

--- 04_JS/for.js
+++ 04_JS/for.js
@@ -16,7 +16,7 @@
 // for (let i = 0; i < 3; i += 1) {
 //   console.log(`상위 for문 입니다. ${i + 1}번째 작동 중`);
 //   for (let j = 0; j < 5; j += 1) {
-//     console.log(`   하위 for문 입니다. i는 ${i}, j는 ${j} 입니다!`)
+//     console.log(`하위 for문 입니다. i는 ${i}, j는 ${j} 입니다!`)
 //   }
 // }
 
