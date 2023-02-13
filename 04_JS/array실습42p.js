a09b339b6992c68d2bf028901c36d234a0b4a304
sang ah <kr.sangah@gmail.com>
2023-02-13 16:33:35 +0900

    230213

--- 04_JS/array실습42p.js
+++ 04_JS/array실습42p.js
@@ -1,45 +1,47 @@
 // 14. JS LECTURE (P.42 실습)
 
-// 0, 1, 2~ 0부터 시작하는 점
+// • kdt 라는 변수에 자신과 같은 줄에 앉은 사람들의 이름이 담긴 배열을 만들기
+// • 3번째 참여자의 이름을 출력하기
 
-let kdt = ['두루', '인영', '상아', '유림'];
+let kdt = ['두루', '인영', '상아', '유림', '슬기'];
+console.log(kdt.push[2])
 
-console.log(kdt.push('백진솔'));
-// 앞에 6 뜨고, 이름 정렬된다. 주의!
-console.log(kdt);
+// console.log(kdt.push('백진솔'));
+// // 앞에 6 뜨고, 이름 정렬된다. 주의!
+// console.log(kdt);
 
-// kdt.pop();
-console.log(kdt.pop());
-// 백진솔 하나만 뜸. 어떤 값을 빼냈는지만 나옴
-console.log(kdt);
-
-
-// kdt[0] = '진솔'
-// console.log(kdt.length);
+// // kdt.pop();
+// console.log(kdt.pop());
+// // 백진솔 하나만 뜸. 어떤 값을 빼냈는지만 나옴
+// console.log(kdt);
 
 
-// const kdt = ['두루', '인영', '상아', '유림', '슬기'];
-// console.log(kdt[2])
+// // kdt[0] = '진솔'
+// // console.log(kdt.length);
 
-kdt.unshift('효석');
-console.log(kdt);
 
-kdt.shift('효석');
-console.log(kdt);
+// // const kdt = ['두루', '인영', '상아', '유림', '슬기'];
+// // console.log(kdt[2])
 
-console.log(kdt.unshift('효석'));
-// 5 뜸
-console.log(kdt);
+// kdt.unshift('효석');
+// console.log(kdt);
 
-console.log(kdt.shift(''));
-// 효석 만 뜸. 어떤 값을 빼냈는지 나옴
-console.log(kdt);
+// kdt.shift('효석');
+// console.log(kdt);
 
-for (let i = 0; i < kdt.length; i++) {
-  console.log(kdt[i]);
-}
+// console.log(kdt.unshift('효석'));
+// // 5 뜸
+// console.log(kdt);
 
-// 배열에 내장된 함수들이 더 많이 있는데,
-// 그건 아예 따로 다룰 것임. 꽤나 중요하다.
-// 실제로 데이터베이스 받아와서 처리하는거 많다.
-// 코테에서도 주구장창 쓰기 때문에..
+// console.log(kdt.shift(''));
+// // 효석 만 뜸. 어떤 값을 빼냈는지 나옴
+// console.log(kdt);
+
+// for (let i = 0; i < kdt.length; i++) {
+//   console.log(kdt[i]);
+// }
+
+// // 배열에 내장된 함수들이 더 많이 있는데,
+// // 그건 아예 따로 다룰 것임. 꽤나 중요하다.
+// // 실제로 데이터베이스 받아와서 처리하는거 많다.
+// // 코테에서도 주구장창 쓰기 때문에..
---
+++ 04_JS/class/q.128.js
@@ -0,0 +1,26 @@
+class HydroCar extends Car {
+  constructor(brand, color, fuel) { }
+  super(brand, color);
+  this.fuel = fuel;
+}
+
+showSpec() {
+  super.showSpec();
+  console.log(`그리고 이 차는 ${this.fuel}의 힘으로 움직입니다!`);
+}
+}
+
+const hyundai = new Car('hyundai', 'white');
+const tesla = new ElecCar('tesla', 'red', 'const
+  
+nexxo = new HydroCar nexxo '', skyblue '', '
+console
+log tesla instanceof ElecCar
+console
+log nexxo instanceof ElecCar
+console
+log nexxo instanceof Car
+console
+log nexxo instanceof Object
+Quiz,
+  console.log 는 어떻게 찍힐까요
