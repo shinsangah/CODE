a7e39aaddea6cf962a5b7a83ee1663d587b6f6cc
sang ah <kr.sangah@gmail.com>
2023-02-14 17:52:08 +0900

    230214

--- 04_JS/DOM/todolist.js
+++ 04_JS/DOM/todolist.js
@@ -1,3 +1,4 @@
 const addBtn = document.querySelector(".input-btn");
 const todoList = document.querySelector(".todo-list");
-const inputTask = document.querySelector(".input task");
+const inputTask = document.querySelector(".input-task");
+
