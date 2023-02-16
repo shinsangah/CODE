b6063d81d476e5f0b5e83899afed064626fffb03
sang ah <kr.sangah@gmail.com>
2023-02-16 15:56:24 +0900

    230216

---
+++ 04_JS/DOM/todolist(상아).js
@@ -0,0 +1,55 @@
+const addBtn = document.querySelector(".input-btn") // '추가' 버튼
+const todoList = document.querySelector(".todo-list")
+const inputTask = document.querySelector(".input-task") // 텍스트 입력하는 창
+
+function deleteTask(t) {
+  t.parentNode.remove()
+}
+
+// addList 목록 추가 메서드
+function addList() {
+
+  if (inputTask.value === "") { // 글자버튼 위에 아무런 내용 없이 '추가' 버튼을 눌렀을 시 실행
+    inputTask.setAttribute("placeholder", "내용을 입력하세요"); // placeholder에 속성 추가
+    return; // else 안쓰고 그냥 이렇게 return; 먹여버리고 종료시킬 수 있음
+  };
+
+  const addLi = document.createElement("li"); // todolist 작성한 목록됨
+  const checkBtn = document.createElement("input"); // 체크버튼
+  checkBtn.setAttribute("type", "checkbox");
+  // 미리 addEventListender 걸어줘야 얘가 할 일을 기억하고 붙게 된다.
+  checkBtn.addEventListener("click", function () {
+    if (checkBtn.checked === true) { // .checked 체크카 되어있으면! 내장함수
+      checkBtn.parentNode.style.textDecoration = "line-through";
+    } else {
+      checkBtn.parentNode.setAttribute("style", "text-decoration: none")
+    }
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "삭제";
+
+  // 이벤트 객체를 통해서 누가 delete 버튼이 클릭되었는지 확인
+  // deleteBtn.addEventListener("click", function (e) {
+  //   e.target.parentNode.remove(); // 부모 요소인 li 요소가 지워지게 됨
+  // })
+
+  // [다른 방법]
+  deleteBtn.setAttribute("onclick", "deleteTask(this)")
+  // 삭제 버튼에 onclick 속성 추가해서 this 자기를 삭제시키는 것
+
+  addLi.append(checkBtn); // 체크 버튼
+  addLi.append(inputTask.value); // 체크버튼을 만들고, 그 뒤에 글자칸에 내용
+  addLi.append(deleteBtn); // 삭제 버튼
+
+  todoList.appendChild(addLi);
+  // <li><checkbox/>할 일<삭제버튼></li>
+
+  inputTask.value = ""; // 글자 입력하는 창, 빈 문자열로 민들어서 지워주기
+
+}
+
+// 미리 addEventListender 걸어줘야 얘가 할 일을 기억하고 붙게 된다.
+addBtn.addEventListener("click", addList);
+// '추가' 버튼에 클릭 누르면 목록추가 메서드 실행
+
--- 04_JS/DOM/todolist.js
+++ 04_JS/DOM/todolist.js
@@ -7,17 +7,19 @@ function deleteTask(t) {
 }
 // 아래에 써도 hoisting 으로 문제 없지만 위에 작성
 
-// 버튼이 클릭 됐을 때 실행
+// addList 목록 추가 메서드
 function addList() {
-  if (inputTask.value === "") // 아무런 내용도 없이 '추가' 버튼을 누르면
+  if (inputTask.value === "") // 아무런 내용도 없이 '추가' 버튼을 눌렀을 시 실행
   {
     inputTask.setAttribute("placeholder", "내용을 입력하세요");
-    // return; 그냥 이렇게 먹여버리기도 한다. 그럼 종료가 된다. else 안쓰고 return 해서 함수 종료.
+    // inputTask placeholder에 속성 추가하는 것임
+    // return; 그냥 이렇게 먹여버리기도 한다.
+    // 그럼 종료가 된다. else 안쓰고 return 해서 함수 종료.
     return;
   };
 
-  const addLi = document.createElement("li");
-  const checkBtn = document.createElement("input");
+  const addLi = document.createElement("li"); // todolist 작성한 목록이 되는거임
+  const checkBtn = document.createElement("input"); // 체크 버튼 칸
   checkBtn.setAttribute("type", "checkbox");
   // 미리 addEventListener 걸어줘야 얘가 할 일을 기억하고 붙게된다.
   checkBtn.addEventListener("click", function () {
@@ -47,7 +49,7 @@ function addList() {
 
   addLi.append(checkBtn);
   addLi.append(inputTask.value);
-  // 체크버튼을 만들고, 그 뒤에 글자버튼을 만들어라.
+  // 체크버튼을 만들고, 그 뒤에 글자칸에 내용
   addLi.append(deleteBtn);
 
   todoList.appendChild(addLi);
@@ -58,3 +60,8 @@ function addList() {
 
 }
 addBtn.addEventListener("click", addList);
+// addList 갖다 쓰는게 아니라, 새롭게 객체로써 생성이 되는거임.
+// 그래서 checkBtn 등 새로운 addList 가 만들어지는거고,
+// 어느 addList 소속인지 알려주는거고 삭제할려고 할 때 그래서
+// this 같은 것을 쓰는 것이다. 특정지을 수 있으니까.
+
